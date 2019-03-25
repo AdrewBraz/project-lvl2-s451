346f58f7a3d57e79f9b516734d92d4049f322fd9
AdrewBraz <andbrazhnikov@gmail.com>
2019-03-25 14:37:26 +0300

    First release lint fix

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 
+// eslint-disable-next-line import/no-extraneous-dependencies
 import program from 'commander';
 
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
-  .arguments('<firstConfig> <secondConfig>')
+  .arguments('<firstConfig> <secondConfig>');
 
-program.parse(process.argv)
+program.parse(process.argv);
