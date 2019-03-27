1bef63ef6564ec39b67e7c80e68c471b4de9f153
AdrewBraz <andbrazhnikov@gmail.com>
2019-03-27 20:18:17 +0300

    Naming fix

--- src/parsers.js
+++ src/parsers.js
@@ -3,6 +3,6 @@ import yaml from 'js-yaml';
 const formats = { '.json': JSON.parse, '.yml': yaml.safeLoad };
 
 export default (ext, obj) => {
-  const getParser = formats[ext];
-  return getParser(obj);
+  const pickParser = formats[ext];
+  return pickParser(obj);
 };
