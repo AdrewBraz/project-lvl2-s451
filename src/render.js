f421391cb2a2eb12c5dc2bce3a165f1440ceddde
AdrewBraz <andbrazhnikov@gmail.com>
2019-03-30 16:34:02 +0300

    naming fix

--- src/render.js
+++ src/render.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 
 const addTabs = level => ' '.repeat(2 * level);
 
-const makeLine = (data, level) => {
+const makeValue = (data, level) => {
   if (!(data instanceof Object)) return data;
   const openTab = addTabs(level + 1);
   const closTab = addTabs(level);
@@ -15,16 +15,16 @@ const render = (ast) => {
     const {
       key, value, children, type,
     } = item;
-    const resultLine = makeLine(value, level);
+    const resultValue = makeValue(value, level);
     switch (type) {
       case 'added':
-        return `${tab}+ ${key}: ${resultLine}`;
+        return `${tab}+ ${key}: ${resultValue}`;
       case 'deleted':
-        return `${tab}- ${key}: ${resultLine}`;
+        return `${tab}- ${key}: ${resultValue}`;
       case 'changed':
-        return [`${tab}+ ${key}: ${makeLine(value[0], level + 1)}`, `${tab}- ${key}: ${makeLine(value[1], level + 1)}`];
+        return [`${tab}+ ${key}: ${makeValue(value[0], level + 1)}`, `${tab}- ${key}: ${makeValue(value[1], level + 1)}`];
       case 'unchanged':
-        return `${tab}  ${key}: ${resultLine}`;
+        return `${tab}  ${key}: ${resultValue}`;
       case 'node':
         return `${tab}${key}: {\n${_.flatten(iter(children, level + 1)).join('\n')}\n${tab}}`;
       default:
